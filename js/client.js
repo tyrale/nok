40178d9a2ae48a9b9671b3991d86aaf05532fb99
tyrale <emailme@tyrale.com>
2015-12-08 15:23:53 -0600

    updated button

--- js/client.js
+++ js/client.js
@@ -13,30 +13,4 @@ $(function() {
     showNav : true // Set to false to hide navigation arrows.
   });
 
-  var handler = StripeCheckout.configure({
-    key: 'pk_test_XRcg6z6XeDDI9082dNpOIf9h',
-    image: '/img/documentation/checkout/marketplace.png',
-    locale: 'auto',
-    token: function(token) {
-      // Use the token to create the charge with a server-side script.
-      // You can access the token ID with `token.id`
-    }
-  });
-
-  $('#customButton').on('click', function(e) {
-    // Open Checkout with further options
-    handler.open({
-      name: 'Weareaddsubtract',
-      description: '2 widgets',
-      amount: 2000,
-      shippingAddress: ''
-    });
-    e.preventDefault();
-  });
-
-  // Close Checkout on page navigation
-  $(window).on('popstate', function() {
-    handler.close();
-  });
-
 })
